77b2d19a17cd35ddec80a615dd789160add4150e
Amine <hamdidou.meliani.mm@gmail.com>
2024-04-17 18:30:32 +0100

    first comit

---
+++ script.js
@@ -0,0 +1,12 @@
+const countElement = document.getElementById('number'); 
+let count = 0; 
+
+async function startCounting() {
+  for (let i = 0; i <= 185; i++) { 
+    countElement.textContent = count; 
+    await new Promise((resolve) => setTimeout(resolve, 15)); 
+    count++; 
+  }
+}
+
+window.onload = startCounting;
